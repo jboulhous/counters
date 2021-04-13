e7f7e4613ddefa282a9f48657fc95d59d2ac9dd8
Jamal Boulhous <boulhous@gmail.com>
2021-04-13 22:29:48 +0000

    Introduce testing using fake-indexeddb

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the headline', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Count anything you want/i);
   expect(linkElement).toBeInTheDocument();
 });
+
